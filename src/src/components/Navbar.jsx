7162e798a66e2a5d8b08bb5cfcc1cec73f0a84ab
Victor <victoramarill13@gmail.com>
2022-08-28 14:20:59 -0300

    Navbar standar

--- src/src/components/Navbar.jsx
+++ src/src/components/Navbar.jsx
@@ -1,15 +1,31 @@
-import "../styles/Navbar.css";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
 
-const Navbar = () => {
+export default function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="logo">WortiseBlog</div>
-      <div className="navmenu">
-        <button className="button">Singin</button>
-        <button className="button">Singup</button>
-      </div>
-    </nav>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+      WortiseBlog
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 2 }}>
+          </Typography>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Register</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
-};
-
-export default Navbar;
+}
